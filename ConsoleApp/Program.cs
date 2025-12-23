dcff521e435ab1fed01cdcf5afd5db9ab731a8b9
Dhvani Modi <dhvani.modi@mckesson.ca>
2025-12-23 21:07:53 +0000

    Add .NET Console App project and devcontainer config

---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+void CalculateNumberProperties(int number, out bool isEven, out bool isPrime)
+{
+    isEven = (number % 2 == 0);
+
+    if (number <= 1)
+    {
+        isPrime = false;
+        return;
+    }
+
+    isPrime = true;
+    for (int i = 2; i <= Math.Sqrt(number); i++)
+    {
+        if (number % i == 0)
+        {
+            isPrime = false;
+            break;
+        }
+    }
+}
